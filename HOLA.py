56ac046525afc421323456074f6a03333062982a
Stalin Mendieta <you@example.com>
2025-03-22 13:26:01 -0500

    Agregando hola

---
+++ HOLA.py
@@ -0,0 +1,17 @@
+# Función para saludar a una persona
+def saludar(nombre):
+    print(f"¡Hola, {nombre}! ¡Bienvenido/a!")
+
+# Solicitar el nombre del usuario
+nombre_usuario = input("¿Cómo te llamas? ")
+
+# Llamar a la función de saludo
+saludar(nombre_usuario)# Función para saludar a una persona
+def saludar(nombre):
+    print(f"¡Hola, {nombre}! ¡Bienvenido/a!")
+
+# Solicitar el nombre del usuario
+nombre_usuario = input("¿Cómo te llamas? ")
+
+# Llamar a la función de saludo
+saludar(nombre_usuario)
